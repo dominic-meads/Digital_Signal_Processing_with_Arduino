ef5c57b1af5122bfd5f2b427055721af5a7171bd
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-02-01 19:45:02 -0800

    changed "size" to "length" to indicate array lengt

--- DSP_Arduino_pt3_convolution/DSP_Arduino_pt3_convolution_rshift.ino
+++ DSP_Arduino_pt3_convolution/DSP_Arduino_pt3_convolution_rshift.ino
@@ -3,8 +3,8 @@ implements a right shift of an array (takes all indecies in array, shifts them r
 */
 
 // function declarations /////////////////////////////////////////////////////////////////////////
-void right_shift_arr(int * array, int size);  
-void print_arr(int * array, int size);
+void right_shift_arr(int * array, int leng);  
+void print_arr(int * array, int length);
 
 
 // main /////////////////////////////////////////////////////////////////////////////////////////
@@ -12,14 +12,14 @@ void setup() {
   Serial.begin(9600);  // for testing using printing functions
   
   int my_array [10] = {1,2,3,4,5,6,7,8,9,10}; 
-  int my_array_size = 10;
+  int my_array_length = 10;
   
   Serial.print("Original array:\n");
-  print_arr(my_array,my_array_size);
+  print_arr(my_array,my_array_length);
   
   Serial.print("array shifted right 1 place:\n");
-  right_shift_arr(my_array,my_array_size);
-  print_arr(my_array,my_array_size);  
+  right_shift_arr(my_array,my_array_length);
+  print_arr(my_array,my_array_length);  
 }
 
 void loop() {
@@ -30,9 +30,9 @@ void loop() {
 
 // function definitions /////////////////////////////////////////////////////////////////////////
 
-void right_shift_arr(int * array, int size)  
+void right_shift_arr(int * array, int length)  
 {
-	for(int i = size - 1; i > 0; i--)
+	for(int i = length - 1; i > 0; i--)
 	{
 		array[i]=array[i-1];
 	}
@@ -40,9 +40,9 @@ void right_shift_arr(int * array, int size)
 }
 
 	
-void print_arr(int * array, int size)
+void print_arr(int * array, int length)
 {
-  for(int i = 0; i < size; i++)
+  for(int i = 0; i < length; i++)
   {
     Serial.print(array[i]);
     Serial.print(' ');       // just print a space
