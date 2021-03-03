95861aa2f71d67fe91d892c1894a6362cccf3cbf
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-03-02 21:00:31 -0800

    removed stem plot

--- DSP_Arduino_pt4_FIR/Truncated_sinc_plot.m
+++ DSP_Arduino_pt4_FIR/Truncated_sinc_plot.m
@@ -1,8 +1,7 @@
 n = -4*pi:0.05:4*pi;
 x = sinc(n);
 figure;
-plot(n/pi,x); hold on;
-stem(n/pi,x,'LineStyle','none'); axis([-4 4 -0.3 1.1]);
+plot(n/pi,x); axis([-4 4 -0.3 1.1]); grid on;
 title('sinc function'); xlabel('units of pi'); ylabel('sinc(x)');
 
 
@@ -12,6 +11,5 @@ for k = 1:length(x1)
     y1(k) = piecewise1(x1(k));
 end
 figure;
-plot(x1/pi,y1); hold on;
-stem(x1/pi,y1,'LineStyle','none'); axis([-4 4 -0.3 1.1]);
+plot(x1/pi,y1); axis([-4 4 -0.3 1.1]); grid on;
 title('Truncated sinc function'); xlabel('units of pi'); ylabel('sinc(x)');
